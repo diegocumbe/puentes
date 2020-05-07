c250459d0ab0800b8e24b13bb0fcf0544886d18d
diegocumbe <39920177+diegocumbe@users.noreply.github.com>
2020-05-07 14:32:09 -0500

    Update db.php

--- db.php
+++ db.php
@@ -1,11 +1,11 @@
 <?php
-
 session_start();
 
-    $conn = mysqli_connect('db4free.net','
+    $conn = mysqli_connect('db4free.net',
     'puentes123',
     'puentes123',
     'puentes'
 );
+
     
 ?>
